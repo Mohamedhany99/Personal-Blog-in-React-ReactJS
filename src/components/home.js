8dc0dc87e5f33da17efbc23d7bddd69aa384d961
Mohamed Hany <Mohamed.hany182@outlook.com>
2022-11-09 17:51:41 +0200

    Added props with 2 components

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
+import BlogList from "./bloglist";
+
 const Home = () => {
-    const handleClick = (e) =>{
-        console.log("hi hany",e);
-    }
-    const handleClickagain = (name,e) =>{
-        console.log('Hi'+name,e.target);
-    }
-    
+
+    const [blogs,setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2> Home PAGE</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e)=>{handleClickagain('hanyyyy',e);}}>Click Me again</button>
+            <BlogList blogs={blogs} title="All Blogs"/>
+            {/* <h2> Home PAGE</h2>
+            <p> {name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button> */}
+            {/* <button onClick={(e)=>{handleClickagain('hanyyyy',e);}}>Click Me again</button> */}
         </div>
      );
 }
