d8e54b199e94b49d87ef158392c0b9f230c2257b
Mohamed Hany <Mohamed.hany182@outlook.com>
2022-11-15 15:36:35 +0200

    handling errors in requests from api

--- src/components/home.js
+++ src/components/home.js
@@ -5,21 +5,33 @@ const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
     const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
-            fetch("http://localhost:8000/blogs").then(res => {
+            fetch("http://localhost:8000/blogs")
+            .then(res => {
+                console.log(res);
+                if (!res.ok){
+                    throw Error("could not fetch the data (backend problem)");
+                }
             return res.json();
         }).then((data)=> {
             console.log(data);
             setBlogs(data);
             setIsPending(false);
-        })    
+            setError(null);
+        })
+        .catch(err =>{
+            setError(err.message);
+            setIsPending(false);
+        } )    
         }, 1000);
         
     },[]);
     return ( 
         <div className="home">
+            { error && <div> { error } </div> }
             { isPending && <div> Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             {/* <button onClick={() => setName('Hanyyyy')}>Change Name (for use effect hook)</button>
